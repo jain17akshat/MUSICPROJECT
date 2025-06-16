df2303efcc1077abd7dec95c9009887968b75edb
jain17akshat <jain.7akshat@gmail.com>
2025-06-16 18:00:58 +0530

    Add FeaturedCourses component and spotlight animation to enhance course display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses"
 export default function Home(){
   return (
 <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
 <h1 className="text-2xl  text-strong   text-center"></h1>
 <HeroSection/>
+<FeaturedCourses/>
+
 
 </main>
   );
