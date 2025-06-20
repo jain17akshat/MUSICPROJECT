f14fd78800a4116693f29c04ae04b18cdec2615d
jain17akshat <jain.7akshat@gmail.com>
2025-06-20 14:48:17 +0530

    Add Instructors component and AnimatedTooltip for enhanced user engagement; update package.json and package-lock.json to include simplex-noise dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import FeaturedCourses from "@/components/FeaturedCourses"
 import WhyChooseUs from "@/components/WhyChooseUs"
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
-
+import Instructors from "@/components/Instructors";
 export default function Home(){
   return (
 <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -14,7 +14,7 @@ export default function Home(){
 <WhyChooseUs/>
 <MusicSchoolTestimonials/>
 <UpcomingWebinars />
-
+<Instructors/>
 </main>
   );
 }
